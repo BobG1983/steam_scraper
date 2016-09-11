82c088bc5ac29d200e520e1bb423d173cbdf9fdd
Robert Gardner <rantingbob@gmail.com>
2016-09-10 23:16:26 -0700

    Fixed broken dependency on HTTParty due to capitalization

--- lib/steam_scraper.rb
+++ lib/steam_scraper.rb
@@ -2,17 +2,20 @@ require 'steam_scraper/version'
 require 'steam_scraper/game_list_scraper'
 require 'steam_scraper/game_page_scraper'
 
-# Module that provides al SteamScraper functionality
+# Module that provides all SteamScraper functionality
 module SteamScraper
-  def initialize(*_args)
-    @game_list_scraper = GameListScraper.new
-    @game_page_scraper = GamePageScraper.new
-  end
+  # Class actually scrapes the Steam Store
+  class SteamScraper
+    def initialize(*_args)
+      @game_list_scraper = GameListScraper.new
+      @game_page_scraper = GamePageScraper.new
+    end
 
-  def scrape(first_page = 1, last_page = nil)
-    scraped_game_list = @game_list_scraper.scrape(first_page, last_page)
-    final_game_list = @game_page_scraper.scrape(scraped_game_list)
+    def scrape(first_page = 1, last_page = nil)
+      scraped_game_list = @game_list_scraper.scrape(first_page, last_page)
+      final_game_list = @game_page_scraper.scrape(scraped_game_list)
 
-    final_game_list
+      final_game_list
+    end
   end
 end
