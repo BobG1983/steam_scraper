61892955cf25e41bab37a254be23dda5ccad2bc0
Robert Gardner <rantingbob@gmail.com>
2016-09-15 01:01:41 -0700

    Updated to now handle age check pages

--- lib/steam_scraper/page_retriever.rb
+++ lib/steam_scraper/page_retriever.rb
@@ -4,9 +4,19 @@ require 'nokogiri'
 
 # Class for retrieving a Nokogiri tree from a URL
 class PageRetriever
+  def get_age_check_url(url)
+    url.sub('/app/', '/agecheck/app/')
+  end
+
   def retrieve(url)
     page_contents = HTTParty.get(url)
-    Nokogiri::HTML(page_contents)
+    page = Nokogiri::HTML(page_contents)
+    if page.xpath('//form[@id="agecheck_form"]')
+      url = get_age_check_url(url)
+      page_contents = HTTParty.post(url, body: { ageDay: 1, ageMonth: 1, ageYear: 1980 })
+      page = Nokogiri::HTML(page_contents)
+    end
+    page
   rescue
     nil
   end
